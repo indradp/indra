87777e4cc6729c70a6c85168289a4f897b42c635
rifqiaziz <rifqi.mozart@gmail.com>
2014-05-08 23:04:06 +0700

    tugas indra 6306110044

---
+++ makulpengujiand3-master/C#/UnitTestProjectIndra/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculatorv1;
+
+namespace UnitTestProjectIndra
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        double a = 21;
+        double b = 7;
+        MainWindow mw = new MainWindow();
+        
+        [TestMethod]
+        public void TestMethodTambah()
+        {
+            double actual = 28;
+            double expected = mw.Tambah(a, b);
+            Assert.AreEqual(actual, expected);
+
+        }
+
+        [TestMethod]
+        public void TestMethodKurang()
+        {
+            double actual = 14;
+            double expected = mw.Kurang(a, b);
+            Assert.AreEqual(actual, expected);
+
+        }
+
+        [TestMethod]
+        public void TestMethodKali()
+        {
+            double actual = 147;
+            double expected = mw.Kali(a, b);
+            Assert.AreEqual(actual, expected);
+
+        }
+
+        [TestMethod]
+        public void TestMethodBagi()
+        {
+            double actual = 3;
+            double expected = mw.Bagi(a, b);
+            Assert.AreEqual(actual, expected);
+
+        }
+    }
+}
